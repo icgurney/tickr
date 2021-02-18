b1b599d90a2021bafd1c359979be362ffc3fa7c1
Ian Gurney <ianclaytongurney@gmail.com>
2021-02-18 00:53:25 -0600

    component scan

--- src/main/java/edu/mccnebu/tickr/TickrApplication.java
+++ src/main/java/edu/mccnebu/tickr/TickrApplication.java
@@ -2,8 +2,10 @@ package edu.mccnebu.tickr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({ "edu.mccnebu.tickr.*" })
 public class TickrApplication {
 
 	public static void main(String[] args) {
